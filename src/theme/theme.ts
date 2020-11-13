8919db5cb3d326df44dc5f992e004600727786ac
DanielBerrioB <daniberrio10@hotmail.com>
2020-11-13 14:56:48 -0500

    Finishin setup project

---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+export type ThemeType = typeof theme; // This is the type definition for my theme object.
+
+const theme = {
+  colors: {
+    primary: "#0998B7", //Base color
+    secondary: "#00B1BB",
+    variant: "#13C9B1",
+  },
+  text: "#000",
+  background: "#fff",
+};
+
+export default theme;
